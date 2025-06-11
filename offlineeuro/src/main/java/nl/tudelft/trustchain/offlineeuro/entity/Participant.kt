2900e6e4bcda8f5ffcbd8714a1c8ea0bc964b994
Matei Mirica <M.M.Mirica@student.tudelft.nl>
2025-06-11 20:13:45 +0200

    what was that

--- offlineeuro/src/main/java/nl/tudelft/trustchain/offlineeuro/entity/Participant.kt
+++ offlineeuro/src/main/java/nl/tudelft/trustchain/offlineeuro/entity/Participant.kt
@@ -56,13 +56,14 @@ abstract class Participant(
     }
 
     fun removeRandomness(publicKey: Element) {
-        for (element in randomizationElementMap.entries) {
-            val key = element.key
-
-            if (key == publicKey) {
-                randomizationElementMap.remove(key)
-            }
-        }
+        randomizationElementMap.remove(publicKey)
+//        for (element in randomizationElementMap.entries) {
+//            val key = element.key
+//
+//            if (key == publicKey) {
+//                randomizationElementMap.remove(key)
+//            }
+//        }
     }
 
     fun addCallback(callback : (String?) -> Unit): Unit {
