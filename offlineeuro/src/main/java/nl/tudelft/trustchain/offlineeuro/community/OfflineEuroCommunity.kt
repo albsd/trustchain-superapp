eb5fee973c2bffe2ebe0173b644b75c83e8cd590
Ana Marcu <46790150+AnaCMarcu@users.noreply.github.com>
2025-06-12 10:19:08 +0200

    change community

--- offlineeuro/src/main/java/nl/tudelft/trustchain/offlineeuro/community/OfflineEuroCommunity.kt
+++ offlineeuro/src/main/java/nl/tudelft/trustchain/offlineeuro/community/OfflineEuroCommunity.kt
@@ -66,7 +66,7 @@ class OfflineEuroCommunity(
     database: TrustChainStore,
     crawler: TrustChainCrawler = TrustChainCrawler()
 ) : TrustChainCommunity(settings, database, crawler) {
-    override val serviceId = "ffffd716494b474ea9f614a16a4da0aed6899aec"
+    override val serviceId = "ffffd716494b474ea9f614a16a4da0aed6899aef"
 
     lateinit var messageList: MessageList<ICommunityMessage>
 
